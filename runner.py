b8fe522b3ad734d558e909eb3f2a3df778c0856b
Ashkan Mansouri Yarahmadi <ashkan.my@gmail.com>
2024-12-04 12:49:17 +0100

    Wrote the tokenizer - byte-pair encoding strategy

--- runner.py
+++ runner.py
@@ -1,1 +1,9 @@
-# runner
+from src.loader.PrepareLoader import create_dataloader_v1
+
+with open("/Users/mansoash/LLM/src/db/story.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
---
+++ src/loader/PrepareLoader.py
@@ -0,0 +1,10 @@
+import tiktoken
+from src.loader.PrepareDataset import PrepareDataset
+from torch.utils.data import DataLoader
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = PrepareDataset(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
+    return dataloader
