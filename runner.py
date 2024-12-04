96a9aeb69ee7e83e314d4fd8bf4f8678cd12e4c6
Ashkan Mansouri Yarahmadi <ashkan.my@gmail.com>
2024-12-04 16:37:25 +0100

    Added logging possibility in case db does not exist

--- runner.py
+++ runner.py
@@ -1,8 +1,22 @@
 from src.loader.PrepareLoader import create_dataloader_v1
+from loguru import logger
 
-with open("/Users/mansoash/LLM/src/db/story.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
+pathString = '/Users/mansoash/LLM/src/db/story.txt'
+logger.add('/Users/mansoash/LLM/src/logs/logs.log')
 
+@logger.catch
+def readData(pathString):    
+    with open(pathString, "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    return raw_text
+
+
+
+
+try:
+    raw_text = readData(pathString)
+except:
+    logger.error("no file exists!")
 dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
