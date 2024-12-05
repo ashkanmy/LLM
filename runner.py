11b5a10b2b9684f50169de6f031b3e3066a98ad3
Ashkan Mansouri Yarahmadi <ashkan.my@gmail.com>
2024-12-05 20:18:51 +0100

    Commented the operations

--- runner.py
+++ runner.py
@@ -12,11 +12,12 @@ def readData(pathString):
 
 
 
-
+# catch the error
 try:
     raw_text = readData(pathString)
 except:
     logger.error("no file exists!")
+ # load dataset   
 dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
